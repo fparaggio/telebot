4b76cb412cb66d81e2ed09f6cbd0c38ac260a1a6
Francesco Paraggio <fparaggio@gmail.com>
2017-05-15 23:44:21 +0200

    modified message to prevent further event processing - @andrejs in telebot telegram group

--- plugins/askUser.js
+++ plugins/askUser.js
@@ -9,22 +9,24 @@ module.exports = {
     id: 'askUser',
     plugin(bot) {
 
-        // On every message
-        bot.on('*', (msg, props) => {
+        bot.mod('message', (data) => {
+
+            const {message: msg, props} = data;
+            const emptyMessage = { message: {}, props: {} };
 
             const id = msg.from.id;
             const ask = userList[id];
 
-            // If no question, then it's a regular message
-            if (!ask) return;
+            if (ask) {
+                delete userList[id];
+                emptyMessage.promise = bot.event('ask.' + ask, msg, props);
+                return emptyMessage;
+            }
 
-            // Delete user from list and send custom event
-            delete userList[id];
-            bot.event('ask.' + ask, msg, props);
+            return data;
 
         });
 
-        // Before call sendMessage method
         bot.on('sendMessage', (args) => {
 
             const id = args[0];
@@ -32,7 +34,6 @@ module.exports = {
 
             const ask = opt.ask;
 
-            // If "ask" in options, add user to list
             if (ask) userList[id] = ask;
 
         });
